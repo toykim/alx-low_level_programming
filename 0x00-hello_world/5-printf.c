f2313b199f7ad245071d563912b0017cba1cf978
toykim <toyosiraji47@gmail.com>
2023-06-20 01:50:01 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * main - printing output with  printf
- *
- * Retun: always (0)
- *
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n";
-        return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
