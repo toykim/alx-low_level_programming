2b03a745824e592d310e1a2b2212d1680a09cb90
toykim <toyosiraji47@gmail.com>
2023-07-18 06:26:45 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+/**
+ * main- Entry point
+ * Return: Always 0
+ */
+
 int main(void)
 {
-	char letter = 'a';
+	char letter;
 
+	letter = 'a';
 	while
 		(letter <= 'z'){
 			putchar(letter);
 			letter++;
 		}
 	putchar('\n');
-
 	return (0);
-
 }
